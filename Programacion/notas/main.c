750afa401056cc0657e82d47f731ab85b53fc3f7
Joshua Cardenas <joshua-cardenas@hotmail.com>
2019-03-04 17:38:08 -0500

    Notas ompleto

--- Programacion/notas/main.c
+++ Programacion/notas/main.c
@@ -3,23 +3,26 @@
 
 int main()
 {
-    int a;
+    int a,i;
     printf("Ingrese el numero de notas:\n");
     scanf("%d",&a);
-    float Nota[a];
-    float suma,prom;
-    int n,i;
-    for(i=0,n=a;i<n;i++)
+    float Nota[a],suma;
+    for(i=0,suma=0;i<a;i++)
     {
-    printf("Ingrese nota:\n");
+    printf("Ingrese nota %i:\n",i+1);
     scanf("%f",&Nota[i]);
-    }
-    for(i=0,suma=0;i<n;i++)
-    {
     suma+=Nota[i];
     }
-    prom=suma/n;
-    printf("%f\n",prom);
+    if(suma/a > 5 || suma/a < 0){
+    printf("Dato invalido\n");
+    }else if(suma/a >= 3 && suma/a<=5){
+    printf("Gano : %.2f\n",suma/a);
+    }else if(suma/a >= 2 && suma/a < 3){
+    printf("Habilita: %.2f\n",suma/a);
+    }else if(suma/a >=0 && suma/a < 2){
+    printf("Perdio:%.2f\n",suma/a);
+    }
+    //printf("Su promedio es: %.2f\n",suma/a);
 
     return 0;
 
